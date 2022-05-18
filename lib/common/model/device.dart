12f99af578b00fdc217b7cd9b5f73bdc00f6be9a
macbook_air <tuananhpham0706@gmail.com>
2022-05-18 13:37:50 +0700

    update 17

---
+++ lib/common/model/device.dart
@@ -0,0 +1,26 @@
+class Device {
+  String name;
+  String image;
+  String button;
+  int state;
+  Device({required this.name,required this.image,required this.state,required this.button});
+
+
+
+
+  Device.fromJson(Map<dynamic, dynamic> json)
+      : name = json['name'] as String,
+        image = json['image'] as String,
+        button = json['button'] as String,
+        state = json['state'] as int;
+
+  Map<dynamic, dynamic> toJson() => <dynamic, dynamic>{
+    'name': name,
+    'image': image,
+    'button': button,
+    'state': state
+  };
+}
+
+
+
--- lib/features/home/ui/home_page_ui.dart
+++ lib/features/home/ui/home_page_ui.dart
@@ -19,42 +19,115 @@ class HomePageUI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(padding: EdgeInsets.fromLTRB(16.sp, ScreenUtil().statusBarHeight + 27.sp, 16.sp, 10.sp),
-              color: SmartHomeStyle.primaryColor,
-              child: Row(mainAxisAlignment: MainAxisAlignment.end, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Expanded(child: TextCustom('Home',
-                    size: 20.sp, weight: SmartHomeStyle.mediumWeight)),
-                Row(children: [
-                  IconAction(menuAction, 'assets/images/theme/ic_menu.png',
-                      width: 22.sp, height: 22.sp, padding: 3.sp, color: Colors.white)
-                ])
-              ])),
-          Container(
-            color: Colors.blue,
-            width: 360.sp,
-            height: 200.sp,
-            child: Column(
-              children: [
-                SizedBox(
-                  child: Column(
-                    children: [
-                      TextCustom('Nhiệt độ hiện tại',
-                        size: 17.sp, weight: SmartHomeStyle.mediumWeight,color: Colors.black,),
-                      TextCustom('29*C',
-                          size: 20.sp, weight: SmartHomeStyle.mediumWeight),
+        return Stack(
+          children: [
+            SizedBox(width: 1.sw, height: 1.sh, child:
+            Image.asset('assets/images/theme/ic_livingroom3.png', fit: BoxFit.fill)
+            ),
+            Column(crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(padding: EdgeInsets.fromLTRB(16.sp, ScreenUtil().statusBarHeight + 27.sp, 16.sp, 10.sp),
+                      color: SmartHomeStyle.primaryColor,
+                      child: Row(mainAxisAlignment: MainAxisAlignment.end, crossAxisAlignment: CrossAxisAlignment.start, children: [
+                        Expanded(child: TextCustom('Home',
+                            size: 20.sp, weight: SmartHomeStyle.mediumWeight)),
+                        Row(children: [
+                          IconAction(menuAction, 'assets/images/theme/ic_menu.png',
+                              width: 22.sp, height: 22.sp, padding: 3.sp, color: Colors.white)
+                        ])
+                      ])),
+                  TextCustom('Thời tiết hiện tại',
+                    size: 20.sp, weight: SmartHomeStyle.mediumWeight,color: Colors.black,),
+                  Container(
 
+                    width: 360.sp,
+                    height: 200.sp,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(const Radius.circular(20)),
+                        color: Colors.blue,
+                        border: Border.all(
+                          width: 2,
+                          color: Colors.black12,
+                        )
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              child: Column(
+                                children: [
+                                  TextCustom('29*C',
+                                      size: 20.sp, weight: SmartHomeStyle.mediumWeight),
+                                  TextCustom('Nhiệt độ hiện tại',
+                                    size: 17.sp, weight: SmartHomeStyle.mediumWeight,color: Colors.black,),
+                                ],
+                              ),
+                            ),
+                            Spacer(),
+                            SizedBox(
+                              child: Column(
+                                children: [
+                                  TextCustom('80%',
+                                      size: 20.sp, weight: SmartHomeStyle.mediumWeight),
+                                  TextCustom('Độ ẩm',
+                                    size: 17.sp, weight: SmartHomeStyle.mediumWeight,color: Colors.black,),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 60,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              child: Column(
+                                children: [
+                                  TextCustom('29*C/27*C',
+                                      size: 20.sp, weight: SmartHomeStyle.mediumWeight),
+                                  TextCustom('Nhiệt độ cao nhất/thấp nhất',
+                                    size: 17.sp, weight: SmartHomeStyle.mediumWeight,color: Colors.black,),
+                                ],
+                              ),
+                            ),
+                            Spacer(),
+                            SizedBox(
+                              child: Column(
+                                children: [
+                                  TextCustom('Có mưa',
+                                      size: 20.sp, weight: SmartHomeStyle.mediumWeight),
+                                  TextCustom('Thủ Đức',
+                                    size: 17.sp, weight: SmartHomeStyle.mediumWeight,color: Colors.black,),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+
+
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      TextCustom('Các phòng:',
+                        size: 20.sp, weight: SmartHomeStyle.mediumWeight,color: Colors.black,),
+                      IconAction(menuAction, 'assets/images/theme/ic_news.png',
+                          width: 22.sp, height: 22.sp, padding: 3.sp, color: Colors.white)
                     ],
                   ),
-                )
 
-
-              ],
-            ),
-          ),
-
-          // Expanded(child: page)
-        ]);
+                  // Expanded(child: page)
+                ])
+          ],
+        );
 
   }
+
 }
